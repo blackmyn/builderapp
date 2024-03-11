956555b9b80b8424c684b3793022284331b4113b
Ivan Siarheyenka <blakkmyn@gmail.com>
2024-03-11 15:05:03 +0300

    add new icon and name, stable version

--- app/src/main/java/com/example/builderapp/MainActivity.kt
+++ app/src/main/java/com/example/builderapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.builderapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.builderapp.R
+import com.example.builderapp.data.CoffeeBuilder
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var buttonOatMilk: Button
+    private lateinit var buttonCowMilk : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,28 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val coffeeWithOatMilk = CoffeeBuilder()
+            .withCoffeeBeans("Arabica")
+            .withMilk("Oat milk")
+            .withSugar("Double sugar")
+            .build()
+        val coffeeWithCowMilk = CoffeeBuilder()
+            .withCoffeeBeans("Robusta")
+            .withDoubleCoffee("Double")
+            .withMilk("Cow milk")
+            .build()
+
+        textView = findViewById(R.id.textView)
+        buttonOatMilk = findViewById(R.id.button4)
+        buttonCowMilk = findViewById(R.id.button3)
+
+        buttonCowMilk.setOnClickListener {
+            textView.text = coffeeWithCowMilk.toString()
+        }
+        buttonOatMilk.setOnClickListener {
+            textView.text = coffeeWithOatMilk.toString()
         }
     }
 }
--- app/src/main/java/com/example/builderapp/data/Coffee.kt
+++ app/src/main/java/com/example/builderapp/data/Coffee.kt
@@ -7,4 +7,27 @@ data class Coffee(
     val coffeeCreamer: String? = null,
     val sugar: String? = null,
     val cinnamon: String? = null,
-    val syrup: String? = null)
+    val syrup: String? = null) {
+    override fun toString(): String {
+    val coffeeInfo = StringBuilder("Coffee: $coffeeBeans")
+        if (doubleCoffee != null) {
+            coffeeInfo.append(" (Double)")
+        }
+        if (milk != null) {
+            coffeeInfo.append("\n Milk: $milk")
+        }
+        if (coffeeCreamer != null) {
+            coffeeInfo.append("\n Coffee creamer: $coffeeCreamer")
+        }
+        if (sugar != null) {
+            coffeeInfo.append("\n Sugar: $sugar")
+        }
+        if (cinnamon != null) {
+            coffeeInfo.append("\n Cinnamon: $cinnamon")
+        }
+        if (syrup != null) {
+            coffeeInfo.append("\n Syrup: $syrup")
+        }
+        return coffeeInfo.toString()
+    }
+}
